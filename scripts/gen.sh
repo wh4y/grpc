1af77a7c04ec52d70a10956942cdbc0c6d06cf7f
Anton Veselev <wh4y@mail.ru>
2023-03-10 00:52:09 +0300

    initial

---
+++ scripts/gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd "$SCRIPT_DIR"
+cd ..
+
+yarn tsproto --path ./src/proto --template ./templates/main.hbs
+yarn cti create ./src -e -b
