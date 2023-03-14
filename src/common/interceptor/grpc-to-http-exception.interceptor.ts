ec3b7a029964cdff4a9194e7a4191f67d2fb2dad
Anton Veselev <wh4y@mail.ru>
2023-03-14 18:11:25 +0300

    quickfix

--- src/common/interceptor/grpc-to-http-exception.interceptor.ts
+++ src/common/interceptor/grpc-to-http-exception.interceptor.ts
@@ -1,6 +1,7 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 import { getHttpExceptionFromGrpc } from './../utils/get-http-exception-from-grpc.util';
 
 @Injectable()
@@ -8,7 +9,13 @@ export class GrpcToHttpInterceptor implements NestInterceptor {
   intercept(_context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(
       catchError((err) => {
-        return throwError(() => getHttpExceptionFromGrpc(err));
+        return throwError(() => {
+          if (!err.code) {
+            return err;
+          }
+
+          return getHttpExceptionFromGrpc(err);
+        });
       }),
     );
   }
